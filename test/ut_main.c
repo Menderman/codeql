24aea413e81a4070c517359237311b346850d806
JohnTsai <mendermanm@gmail.com>
2023-05-23 15:57:35 +0800

    update copilot suggests strcpy

--- test/ut_main.c
+++ test/ut_main.c
@@ -1,4 +1,5 @@
 //take a string input from user and copy it to another string buffer
+//using strcpy function
 
 #include <stdio.h>
 #include <string.h>
@@ -6,14 +7,9 @@
 int main()
 {
     char str1[20], str2[20];
-    int i;
     printf("Enter a string: ");
     scanf("%s", str1);
-    for (i = 0; str1[i] != '\0'; ++i)
-    {
-        str2[i] = str1[i];
-    }
-    str2[i] = '\0';
-    printf("String str2: %s\n", str2);
+    strcpy(str2, str1);
+    printf("The copied string is: %s", str2);
     return 0;
 }
