f949606bc1d8779d55df55912fb312196d33ecd8
JohnTsai <mendermanm@gmail.com>
2023-05-28 20:20:39 +0800

    CWE-78-0

--- test/ut_main.c
+++ test/ut_main.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <limits.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-#define FILENAME "data.csv"
 
 int main(int argc, char *argv[]) {
-    //generate 3 random floats
-    float a = (float)rand() / (float)RAND_MAX;
-    float b = (float)rand() / (float)RAND_MAX;
-    float c = (float)rand() / (float)RAND_MAX;
+    //get the username from the command line
+    char *username = argv[1];
 
-    //convert to string
+    //get the userinfo from the system
+    char command[100] = {0};
     //-copilot next line-
-	char str_a[20], str_b[20], str_c[20];
-    sprintf(str_a, "%f", a);
-    sprintf(str_b, "%f", b);
-    sprintf(str_c, "%f", c);
+	sprintf(command, "cat /etc/passwd | grep %s", username);
 
-
-
-
+    system(command);
 }
